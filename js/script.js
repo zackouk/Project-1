64f7392e490e9b8784679742f36164fae231c97a
Zack Ouk <zackouk3000@gmail.com>
2018-08-29 00:14:46 -0700

    added array of objects

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 // FSJS - Random Quote Generator
 
 // Create the array of quote objects and name it quotes
-
+var quotes = [
+    {quote: 'You know you\'re in love when you can\’t fall asleep because reality is finally better than your dreams.', source: 'Dr.Suess', },
+    {quote: 'Get busy living or get busy dying.', source: 'Stephen King', },         
+    {quote: 'Great mind discuss ideas; average minds discuss events; small minds discuss people.', source: 'Elanore Roosevelt', },
+    {quote: 'Love is a serious mental disease.', source: 'Plato', },
+    {quote: 'I say to you today, my friends, that in spite of the difficulties and fustrations of the moment, I stil have a dream.', source: 'Marin Luther King, Jr.', citation: '(I Have A Dream Speech)', },
+    {quote: 'Only I can change my ife. No one can do it for me', source: 'Carol Burnett', },
+    {quote: 'Set your goals high, and don\'t stop till you get there.', source: 'Bo Jackson', },
+    {quote: 'If you dream it, you can do it.', source: 'Walt Disney', }
+]
 
 
 // Create the getRandomQuuote function and name it getRandomQuote
